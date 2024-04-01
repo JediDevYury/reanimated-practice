c219b8a8b23c28cc1fb32c9bcae1aeb24fece0c3
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2024-04-01 10:35:25 +0400

    init: create first animation using "Reanimated" library

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {useSharedValue, useAnimatedStyle, withSpring, withRepeat, DerivedValue} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
+import {useEffect} from "react";
+
+const SIZE = 100.0;
+
+const handleRotation = (progress: DerivedValue<number>) => {
+  'worklet';
+
+  return `${progress.value * 2 * Math.PI}rad`
+}
 
 export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [{ scale: scale.value }, {
+        rotate: handleRotation(progress),
+      }],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), 3, true)
+    scale.value = withRepeat(withSpring(1), 3, true)
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[{
+        height: SIZE,
+        width: SIZE,
+        backgroundColor: 'blue',
+      }, reanimatedStyle]} />
     </View>
   );
 }
